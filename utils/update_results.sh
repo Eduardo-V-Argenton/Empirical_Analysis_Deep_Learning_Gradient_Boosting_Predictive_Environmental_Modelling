4d18b3f6c226c6d10b9dfbd4bbe44f104c8c1aa0
Eduardo Vidoretti Argenton <eduardov.argenton@outlook.com>
2025-05-18 00:33:35 -0300

    correção update_results.sh

--- utils/update_results.sh
+++ utils/update_results.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#
 for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/); do
   if [ "$branch" != "main" ]; then
     # Cria uma pasta temporária com o conteúdo dos logs dessa branch
-    git checkout "$branch" -- results/"$branch"/
+    git checkout "$branch" -- ../results/"$branch"/
   fi
 done
